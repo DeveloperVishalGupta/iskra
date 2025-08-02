4c0b7b8ec8b4b499d108f1e54f1e67b97111b59c
Vishal Gupta <vishalguptaramasare@gmail.com>
2025-08-03 00:12:06 +0530

    build the project and re update

--- app/distribute-your-music/layout.jsx
+++
@@ -1,7 +0,0 @@
-export default function DistributeMusicLayout({ children }) {
-  return (
-    <section className=" items-center justify-center gap-4 py-8 md:py-10">
-      <div className="text-center ">{children}</div>
-    </section>
-  );
-}
